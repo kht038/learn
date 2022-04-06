cd820a8a889c2fa601cb45475029a9bf715030bb
kht038 <kht038@naver.com>
2022-04-06 18:56:16 +0900

    Initial commit

---
+++ 5/list-datatype.py
@@ -0,0 +1,10 @@
+# list_name = [data, a, b, c]
+
+# data append list.append(data)
+# 데이터 할당 - list[index] = data
+# delete data = del list[index
+# list[start:end + 1] - list slicing
+# list[:] list all
+# len(list) - list length
+# list.sort() - list sorting
+# list.sort(reverse=True) 역순 정렬
---
+++ 7/dictionary.py
@@ -0,0 +1,21 @@
+# 시퀀스 자료형
+# 키와 데이터를 가지고있는 사전형 자료형
+# 사전 형태의 자료를 만들때 편리
+
+# dictionary = {key1:data1, key2:data2, key3:data3}
+
+# dictionary[key] = data # 할당하기
+# del dictionary[key] # 삭제하기
+
+# dictionary function
+
+# 키와 데이터쌍 dictionary.items()
+# 키 dictionary.keys()
+# 값 dictionary.values()
+
+# 키 하나에 여러개의 데이터를 담기 위해 리스트나 튜플을 사용할 수 있다.
+# key : (1,2,3)
+# key2 : [1,2,3]
+
+# 딕셔너리 키에 딕셔너리 자료형을 또 담을 수 있다. 여러개를 담기 위해서 라면 반점으로 구분한다.
+# key : {key1 : 3000}
